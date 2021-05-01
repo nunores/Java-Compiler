36b3a398c4fee964d9522515fbaedc33847d05b0
Marina <up201806787@fe.up.pt>
2021-05-02 00:42:47 +0100

    jasmin methods

--- src/jasmin/JasminGenerator.java
+++ src/jasmin/JasminGenerator.java
@@ -1,6 +1,7 @@
 package jasmin;
 
 import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Method;
 
 public class JasminGenerator {
     private ClassUnit classUnit;
@@ -12,6 +13,26 @@ public class JasminGenerator {
     public String classToJasmin(){
         String jasminString = "JASMIN: \n\n" ;
         String stringBuilder = ".class " + classUnit.getClassName() + "\n.super java/lang/Object\n";
-        return jasminString + stringBuilder;
+        return jasminString + stringBuilder + methodToJasmin();
+    }
+
+    public String methodToJasmin(){
+        String stringBuilder = "";
+        for (Method method: classUnit.getMethods()){
+            stringBuilder += "\n.method public ";
+            if (method.isConstructMethod()){
+                stringBuilder += "<init>()V\naload_0\ninvokespecial java/lang/Object.<init>()V\nreturn";
+            }
+            if(method.isStaticMethod()){
+                stringBuilder += "static main([Ljava/lang/String;)V";
+            }
+            else{
+                stringBuilder += method.getMethodName(); 
+            }
+
+            stringBuilder += "\n.end method\n";
+        }
+        return stringBuilder;
+        
     }
 }
